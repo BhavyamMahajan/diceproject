03bf4ccd3ac1671685a4574dbf011746baf07bf1
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2021-05-17 12:51:01 +0530

    Added Sound

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function diceroll()
 {
-    var x = (Math.ceil(Math.random()*6));
-    var showBox = document.querySelector('#div2');
-    x = parseInt(x);
-
-    showBox.innerHTML = '<img src=images/'+x+'.jpg />'
+    var x = parseInt(Math.ceil(Math.random()*6));
+    var y = parseInt(Math.ceil(Math.random()*6));
+    var dice1 = document.querySelector('#dice1');
+    var dice2 = document.querySelector('#dice2');
+    var audio = document.getElementById("audio");
+    audio.play();
+    dice1.innerHTML = '<img src=images/'+x+'.jpg />'
+    dice2.innerHTML = '<img src=images/'+y+'.jpg />'
 }
